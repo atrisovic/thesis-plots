6a5675bc843a6b7560430a1cdde9ae7e2666e6a1
Ana Trisovic <ana.trisovic@gmail.com>
2018-03-18 23:07:04 +0100

    first plots

---
+++ header.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams["font.family"] = "Times New Roman"
+plt.rcParams['axes.facecolor']='white'
+plt.rcParams['savefig.facecolor']='white'
